12369b27d0efe0090245a91afb3d48d7f43a1176
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2022-12-05 20:50:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var (name, age) = ("Роман", 28);
+            Console.WriteLine("Моё имя {0}", name);
+            Console.WriteLine("Мой возраст {0}", age);
+
+            Console.Write("Ввидите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Ввидите возраст с цифрами: ");
+            age = Convert.ToInt32 (Console.ReadLine());
+                
+
+            Console.WriteLine("Ваше имя {0}", name);
+            Console.WriteLine("Ваш возраст {0}", age);
+
+            Console.ReadKey();
+        }
+    }
+}
